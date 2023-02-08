e9bc2d7cd67341289dbfa18d0598340f576af44c
Graciana Amaranto <xenrelay333@hotmail.com>
2023-02-08 02:27:25 -0300

    Agregar archivos de proyecto.

---
+++ Cupcakes/Models/CupcakeType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cupcakes.Models
+{
+    public enum CupcakeType
+    {
+        Birthday,
+        Turquoise,
+        Chocolate,
+        Strawberry
+    }
+}
